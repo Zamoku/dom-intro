3e3299e1ad620c27fb13ffb722531fd3884e4ee1
Nomzamo <nomzamo.nmuleka@gmail.com>
2022-05-11 09:59:32 +0200

    added an alert for text bill widget

--- js/text-bill.js
+++ js/text-bill.js
@@ -25,6 +25,9 @@ var smsTotal = 0;
 function textBillTotal(){
     // get the value entered in the billType textfield
     var billTypeEntered = billTypeText.value.trim();
+    if(billTypeEntered.toLowerCase() !== "call" || billTypeEntered.toLowerCase() !== "sms"){
+        alert("PLEASE make sure you enter SMS or CALL")
+    }
     // update the correct total
     if (billTypeEntered.toLowerCase() === "call"){
         callsTotal += 2.75
